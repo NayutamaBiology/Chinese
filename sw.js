bd3b6f7500088f40efbe21a9de344d00106a0377
NayutamaBiology <ms6mstk8n8@privaterelay.appleid.com>
2026-09-09 01:26:07 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,21 +1,10 @@
-const CACHE='cn-trainer-v1.1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
-self.addEventListener('install',e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));
-});
-self.addEventListener('activate',e=>{
-  e.waitUntil(
-    caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
+self.addEventListener('install',()=>self.skipWaiting());
+self.addEventListener('activate',event=>{
+  event.waitUntil(
+    caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k))))
+      .then(()=>self.registration.unregister())
+      .then(()=>self.clients.matchAll())
+      .then(clients=>clients.forEach(c=>c.navigate(c.url)))
   );
 });
-self.addEventListener('fetch',e=>{
-  e.respondWith(
-    fetch(e.request).then(resp=>{
-      const copy=resp.clone();
-      caches.open(CACHE).then(c=>c.put(e.request,copy));
-      return resp;
-    }).catch(()=>caches.match(e.request).then(r=>r || caches.match('./index.html')))
-  );
-});
+self.addEventListener('fetch',()=>{});
